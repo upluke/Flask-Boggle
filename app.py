3943bb969f9debfa1b48d5c3420cb2a0bc96b273
upluke <lukeforadream@gmail.com>
2022-03-12 14:09:27 -0800

    add falsk session

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 
 from boggle import Boggle
 
 boggle_game = Boggle()
 
-app = Flask('app')
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "dumbonudumb"
 
 @app.route('/')
 def home():
   new_board=boggle_game.make_board()
+  session['board']=new_board
   return render_template("home.html", new_board=new_board) 
 
 @app.route('/test' )
