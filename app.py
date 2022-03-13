ffb15eb8dd9dae84e5f5aab3d09c4cda1c5e9927
upluke <lukeforadream@gmail.com>
2022-03-12 19:43:52 -0800

    post request

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, request, render_template, session
 
 from boggle import Boggle
@@ -10,14 +11,17 @@ app.config['SECRET_KEY'] = "dumbonudumb"
 @app.route('/')
 def home():
   new_board=boggle_game.make_board()
+  
   session['board']=new_board
-  return render_template("home.html", new_board=new_board) 
+  return render_template("index.html", new_board=new_board) 
 
-@app.route('/test' )
+@app.route('/test', methods=["POST"] )
 def test():
-  # word = request.form['word']
-  word=request.args['word']
+  if request.method == "POST":
+    word = request.form.get('word')
+    print(word)
+#   word=request.args['word']
 #   print("****", request.form)
-  return render_template("test.html", word=word) 
+    return render_template("test.html", word=word) 
  
   
