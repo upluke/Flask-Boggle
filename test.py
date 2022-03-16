b5196cb5b2450dc5f2232ce2d93b619f27c11f75
upluke <lukeforadream@gmail.com>
2022-03-15 19:10:48 -0700

    homepage test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from urllib import response
 from app import app
 from flask import session
 from boggle import Boggle
@@ -7,7 +8,20 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
-    def setUP(self):
-        
-        self.client=app.test_client()
-        app.config['TESTING '] = True
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Make sure information is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'<p>High Score:', response.data)
+            self.assertIn(b'Score:', response.data)
+            self.assertIn(b'Seconds Left:', response.data)
