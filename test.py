77837640af7dd999d5a0b101c8cbfa3451f4f3fc
upluke <lukeforadream@gmail.com>
2022-03-18 22:53:55 -0700

    non eng word

--- test.py
+++ test.py
@@ -45,3 +45,11 @@ class FlaskTests(TestCase):
         self.client.get('/')
         response = self.client.get('/check-word?word=impossible')
         self.assertEqual(response.json['result'], 'not-on-board')
+    
+    def non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get(
+            '/check-word?word=fsjdakfkldsfjdslkfjdlksf')
+        self.assertEqual(response.json['result'], 'not-word')
